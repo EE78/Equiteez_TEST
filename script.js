d1dbf60f1817e1b24a3eb082b0cc6115b75d5e5d
Evgeny Eremin <ereminevgeny78@gmail.com>
2023-07-06 11:46:34 +0700

    add fonts, styles, adaptive, script, modal, fixies

---
+++ script.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById("signUpModal");
+const btn = document.getElementById("openModal");
+const span = document.getElementsByClassName("closeModal")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
